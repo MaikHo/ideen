5f31527e9331b4c26abce4b5d620f3e1dd2a7de2
Maik Hoffmann <maikhoffmann@outlook.com>
2017-05-30 09:15:17 +0200

    Update call_user.php

--- php/call_user.php
+++ php/call_user.php
@@ -1,1 +1,32 @@
+<?php
 
+class myclass {
+    static function say_hello()
+    {
+        echo "Hello!\n";
+    }
+}
+
+$classname = "myclass";
+
+call_user_func(array($classname, 'say_hello'));
+
+// dieser Aufruf ist interessant
+call_user_func($classname .'::say_hello'); // As of 5.2.3
+
+$myobject = new myclass();
+
+call_user_func(array($myobject, 'say_hello'));
+
+/*
+Klassen normal mit autoload laden lassen und methode aufrufen (statische Methoden)
+*/
+call_user_func("myclass::say_hello");// testen
+
+// ansonsten Objekt erzeugen
+$myobject = new myclass();
+
+call_user_func(array($myobject, 'say_hello'));
+
+
+?>
